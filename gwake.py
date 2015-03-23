a9f0ad0020446bf5728b9f5e7909b0738ada2747
Paul Nickerson <pvnick@gmail.com>
2015-03-23 15:07:08 +0100

    things seem to be working now

--- gwake.py
+++ gwake.py
@@ -14,19 +14,20 @@ class ShellController():
     self.shell_hwnd = None
     self.shell_proc = None
     self.shell_is_open = False
-    self.findwindow_title = "/tmp/this_window_name_is_unique"
-    self.shell_executable_path = self.config_parser.get('Gwake', 'ShellExecutablePath')
+    self.findwindow_title = '/tmp/this_window_name_is_unique'
+    self.shell_executable = self.config_parser.get('Gwake', 'ShellExecutable')
     self.shell_font = self.config_parser.get('Gwake', 'ShellFont')
-    self.cygwin_bin_path = self.config_parser.get('Gwake', 'CygwinBinPath')
-    self.shell_open_command = [self.shell_executable_path, '-sl', '1500', '-fn', 
+    self.cygwin_path = self.config_parser.get('Gwake', 'CygwinPath')
+    mywindow_broadcaster_cygwin_path = '/cygdrive/' + os.path.abspath('broadcast-mywindow.sh').replace('\\', '/').replace(':', '', 1)
+    self.shell_open_command = [os.path.join(self.cygwin_path, 'bin', self.shell_executable), '-sl', '1500', '-fn', 
                                self.shell_font, '-bg', 'black', '-fg', 'grey', '-sr', '-e',
-                               './bash.exe', '--rcfile', '/cygdrive/c/cygwin/home/pvnic_000/guake-like_windows_shell/broadcast-mywindow.sh']
-    self.transparent_factor = 0.95
-    self.shell_height = 500
+                               './bash.exe', '--rcfile', mywindow_broadcaster_cygwin_path]
+    self.transparent_factor = self.config_parser.getfloat('Gwake', 'ShellTransparency')
+    self.shell_height = self.config_parser.getint('Gwake', 'ShellHeight')
 
   def show_shell(self):
     if not self.is_shell_proc_running():
-      print "No shell found, openning a new instance"
+      print 'No shell found, openning a new instance'
       self._open_shell()
 
     self.set_transparency()
@@ -43,6 +44,10 @@ class ShellController():
     win32gui.PostMessage(self.shell_hwnd, win32con.WM_LBUTTONDOWN, 0, 0)
     self.shell_is_open = True
 
+  def send_string_keystrokes(self, str):
+    for char in str:
+      win32gui.SendMessage(self.shell_hwnd, win32con.WM_CHAR, ord(char), 0)
+
   def hide_shell(self):
     win32gui.ShowWindow(self.shell_hwnd, win32con.SW_HIDE)
     self.shell_is_open = False
@@ -53,7 +58,7 @@ class ShellController():
     win32gui.SetLayeredWindowAttributes(self.shell_hwnd, 1, int(self.transparent_factor * 255), win32con.LWA_COLORKEY | win32con.LWA_ALPHA)
 
   def toggle_shell(self):
-    print("Toggling shell")
+    print 'Toggling shell'
     if self.shell_is_open:
       self.hide_shell()
     else:
@@ -66,16 +71,17 @@ class ShellController():
     return running
 
   def _open_shell(self):
-    drive_letter = os.getcwd()[0]
-    print("Executing command " + str(self.shell_open_command))
+    print 'Opening shell'
+    print 'Executing command ' + str(self.shell_open_command)
     self.shell_proc = subprocess.Popen(self.shell_open_command, 
-                                       cwd=self.cygwin_bin_path,
+                                       cwd=os.path.join(self.cygwin_path, 'bin'),
                                        creationflags=subprocess.CREATE_NEW_CONSOLE)
     self.shell_hwnd = 0
     while self.shell_hwnd == 0:
-      print "finding window"
       self.shell_hwnd = win32gui.FindWindow(None, 
                                             self.findwindow_title)
+    self.send_string_keystrokes('cd ~/\n')
+
 
 class HotKeyListener:
   TOGGLE_SHELL_MESSAGE = 1
@@ -86,16 +92,16 @@ class HotKeyListener:
     self.shell_controller = ShellController(self.config_parser)
 
   def _register_hotkey(self):
-    print "Registering shell toggle hotkey, " + str(self.toggle_shell_key) + ", to message " + str(self.TOGGLE_SHELL_MESSAGE)
+    print 'Registering shell toggle hotkey, ' + str(self.toggle_shell_key) + ', to message ' + str(self.TOGGLE_SHELL_MESSAGE)
     success = windll.user32.RegisterHotKey(None, self.TOGGLE_SHELL_MESSAGE, None, self.toggle_shell_key)
     if not success:
-      raise Exception("Could not register shell toggle hot key")
+      raise Exception('Could not register shell toggle hot key')
 
   def _unregister_hotkey(self):
-    print "Unregistering shell toggle hotkey attached to message " + str(self.TOGGLE_SHELL_MESSAGE)
+    print 'Unregistering shell toggle hotkey attached to message ' + str(self.TOGGLE_SHELL_MESSAGE)
     success = windll.user32.UnregisterHotKey(None, self.TOGGLE_SHELL_MESSAGE)
     if not success:
-      raise Exception("Could not unregister shell toggle hot key")
+      raise Exception('Could not unregister shell toggle hot key')
 
   def listen(self):
     try:
@@ -109,14 +115,14 @@ class HotKeyListener:
         windll.user32.TranslateMessage(byref(msg))
         windll.user32.DispatchMessageA(byref(msg))
     except Exception as ex:
-      print "Caught exception: " + str(ex)
+      print 'Caught exception: ' + str(ex)
     finally:
       self._unregister_hotkey()
 
 
-if __name__ == "__main__":
-  config_parser = ConfigParser.ConfigParser()
-  config_file_path = "gwake.conf"
+if __name__ == '__main__':
+  config_parser = ConfigParser.RawConfigParser()
+  config_file_path = 'gwake.conf'
   config_parser.read(config_file_path)
   hotkey_listener = HotKeyListener(config_parser)
   hotkey_listener.listen()
